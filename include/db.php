88b938d214101efb96832af115ff63fc989befe9
Chris Miles <akkadius1@gmail.com>
2020-05-01 14:28:41 -0500

    Feature/optimize loading prefetching (WIP) (#6)
    * Optimize and prefetch data initial commit
    
    * Move javascript functions / add timings
    
    * Adjust some logic
    
    * Add preloading for factions
    
    * Remove accessing stores directly and hide behind repository
    
    * Cleanup
    
    * Remove reliance on a table for race constants
    
    * Fix signatures
    
    * Get bazaar working again
    
    * More refinements
    
    * Change namespace to CharBrowser
    
    * Fix 2 more missing references to Magelo

--- include/db.php
+++ include/db.php
@@ -7,16 +7,16 @@
  *   (at your option) any later version.
  *
  *   Portions of this program are derived from publicly licensed software
- *   projects including, but not limited to phpBB, Magelo Clone, 
+ *   projects including, but not limited to phpBB, Magelo Clone,
  *   EQEmulator, EQEditor, and Allakhazam Clone.
  *
  *                                  Author:
- *                           Maudigan(Airwalking) 
+ *                           Maudigan(Airwalking)
  *
- * 
+ *
  *   January 7, 2018 - Maudigan
- *      Database performance code wasn't running because the template 
- *      class name was wrong. 
+ *      Database performance code wasn't running because the template
+ *      class name was wrong.
  *   January 7, 2018 - Athrogate/Maudigan
  *      Athrogate sent some code for MySQLi conversion, I added to that
  *      to make it more object oriented. $cbsql is now an object that
@@ -34,7 +34,7 @@ if ( !defined('INCHARBROWSER') )
 
 /*********************************************
                  INCLUDES
-*********************************************/ 
+*********************************************/
 include_once(__DIR__ . "/language.php");
 include_once(__DIR__ . "/functions.php");
 include_once(__DIR__ . "/config.php");
@@ -44,27 +44,30 @@ include_once(__DIR__ . "/template.php");
 /*********************************************
            DATABASE WRAPPER CLASS
    all MySQL calls route through this class
-*********************************************/ 
+*********************************************/
 class Charbrowser_SQL
 {
    //local database handle
    var $_mysql_handle;
-   
-   //holds performance data about queries
-   var $_dbp_performance = array();   
-   
 
-   
+   //holds performance data about queries
+   var $_dbp_performance = array();
+
+   //holds data about which tables have been accessed
+   var $_dbp_tables = array();
+
+
+
    //-------------------------------------
    //            CONSTRUCTOR
    //-------------------------------------
    function __construct($host, $user, $pass, $database)
    {
-      $this->_mysql_handle = New mysqli($host, $user, $pass, $database); 
+      $this->_mysql_handle = New mysqli($host, $user, $pass, $database, $port);
    }
 
-   
-   
+
+
    //-------------------------------------
    //            DESTRUCTOR
    //-------------------------------------
@@ -72,22 +75,22 @@ class Charbrowser_SQL
    {
       $this->_mysql_handle->close();
    }
-   
-   
-   
+
+
+
    //-------------------------------------
    //          CONNECT ERROR
-   //-------------------------------------  
+   //-------------------------------------
    function connect_error()
    {
       return $this->_mysql_handle->connect_error;
-   }   
+   }
 
-   
-   
+
+
    //------------------------------------
    //          FETCH QUERIES
-   // fetches performance data about the 
+   // fetches performance data about the
    // queries that have been run
    //------------------------------------
    function dbp_fetch_queries()
@@ -102,44 +105,91 @@ class Charbrowser_SQL
    // fetches query data parsed into the
    // database_performance_body template
    //------------------------------------
-   function dbp_fetch_parsed()
-   {  
+   function dbp_fetch_parsed($type)
+   {
       //create our own template class instance so we don't interfere with the global one
       $dbp_template = new CB_Template(__DIR__ . "/../templates/" . $cb_override_template_dir, __DIR__ . "/../templates/default");
-      
+
       //load the template
       $dbp_template->set_filenames(array('database_performance' => 'database_performance_body.tpl'));
-      
+
       //skip it if its empty
       if (is_array($this->_dbp_performance))
       {
+         //load the top level items into the array
+         $dbp_template->assign_vars(array(
+            'TYPE' => $type)
+         );
+
          //loop through each query
          foreach ($this->_dbp_performance as $dbp_query)
          {
             //load the top level items into the array
-            $dbp_template->assign_block_vars("query", array( 
+            $dbp_template->assign_block_vars("query", array(
                'QUERY' => $dbp_query['QUERY'],
                'TIME' => $dbp_query['TIME'])
             );
-            
+
             //loop through the explanation and load its data into the array
             foreach ($dbp_query['EXPLANATIONS'] as $dbp_explanation)
             {
                $dbp_template->assign_block_vars("query.explanation", $dbp_explanation);
             }
          }
-      
+
       }
-      
+
       //grab the output
       $dbp_output = $dbp_template->pparse_str('database_performance');
-         
+
       //cleanup
       $dbp_template->destroy;
-      
+
       return $dbp_output;
    }
-   
+
+
+
+   //------------------------------------
+   //        FETCH PARSED TABLES
+   // fetches query data parsed into the
+   // database_performance_body template
+   //------------------------------------
+   function dbp_table_fetch_parsed($type)
+   {
+      //create our own template class instance so we don't interfere with the global one
+      $dbp_template = new CB_Template(__DIR__ . "/../templates/" . $cb_override_template_dir, __DIR__ . "/../templates/default");
+
+      //load the template
+      $dbp_template->set_filenames(array('database_table_performance' => 'database_table_performance_body.tpl'));
+
+      //skip it if its empty
+      if (is_array($this->_dbp_tables))
+      {
+         //load the top level items into the array
+         $dbp_template->assign_vars(array(
+            'TYPE' => $type)
+         );
+
+         //loop through each query
+         $tables = array();
+         foreach ($this->_dbp_tables as $dbp_table)
+         {
+            //load the top level items into the array
+            $dbp_template->assign_block_vars("tables", $dbp_table);
+         }
+
+      }
+
+      //grab the output
+      $dbp_output = $dbp_template->pparse_str('database_table_performance');
+
+      //cleanup
+      $dbp_template->destroy;
+
+      return $dbp_output;
+   }
+
 
 
    //------------------------------------
@@ -148,7 +198,7 @@ class Charbrowser_SQL
    function escape_string($string)
    {
       return $this->_mysql_handle->real_escape_string($string);
-   } 
+   }
 
 
 
@@ -159,36 +209,36 @@ class Charbrowser_SQL
    // in the config.php
    //------------------------------------
    function query($query)
-   {      
+   {
       //is this a select?
       $select = !strncmp("SELECT", strtoupper(trim($query)), 6);
-      
-      //if we aren't monitoring database performance just 
+
+      //if we aren't monitoring database performance just
       //return the query results, don't explain non-selects either
       if (!defined('DB_PERFORMANCE') || !$select) return $this->_mysql_handle->query($query);
-      
-      
-      
+
+
+
       //gather query performance stats
       //do the query with a timer
       timer_start('query');
       $return = $this->_mysql_handle->query($query);
       $time = timer_stop('query');
-      
+
       //report errors
       if (!$return)
       {
          cb_message_die($language['MESSAGE_ERROR'], $this->_mysql_handle->error);
       }
-      
+
       //get an explanation of the query
       $result = $this->_mysql_handle->query("EXPLAIN ".$query);
-      
+
       //load the results into an array
       $explanations = array();
       while($row = $result->fetch_array())
-      { 
-         $explanations[] = array( 
+      {
+         $explanations[] = array(
             'SELECT_TYPE' => $row['select_type'],
             'TABLE' => $row['table'],
             'TYPE' => $row['type'],
@@ -200,16 +250,46 @@ class Charbrowser_SQL
             'EXTRA' => $row['Extra']
          );
 
+         //track how many queries were done on this table
+         if (array_key_exists($row['table'], $this->_dbp_tables))
+         {
+            $this->_dbp_tables[$row['table']]['COUNT'] = $this->_dbp_tables[$row['table']]['COUNT'] + 1;
+            $this->_dbp_tables[$row['table']]['ROWS'] = $this->_dbp_tables[$row['table']]['ROWS'] + $row['rows'];
+         }
+         else
+         {
+            $this->_dbp_tables[$row['table']] = array(
+               'TABLE' => $row['table'],
+               'COUNT' => 1,
+               'ROWS' => $row['rows']
+            );
+         }
+
       }
-         
+
       //cache the performance
-      $this->_dbp_performance[] = array( 
+      $this->_dbp_performance[] = array(
          'QUERY' => $query,
          'TIME' => $time,
          'EXPLANATIONS' => $explanations
       );
-      
+
       return $return;
+   }
+
+   //------------------------------------
+   //    MYSQL FIELD QUERY WRAPPER
+   //------------------------------------
+   function field_query($field, $query)
+   {
+      $result = $this->query($query);
+
+      if ($row = $result->fetch_array())
+      {
+         return $row[$field];
+      }
+
+      return false;
    }
 
 
@@ -230,7 +310,7 @@ class Charbrowser_SQL
    function rows($result)
    {
       return $result->num_rows;
-   } 
+   }
 
 
 
@@ -241,13 +321,32 @@ class Charbrowser_SQL
    {
       return $result->fetch_array();
    }
+
+   //------------------------------------
+   //      MYSQL FETCH ALL WRAPPER
+   //------------------------------------
+   function fetch_all($result)
+   {
+      return $result->fetch_all(MYSQLI_ASSOC);
+   }
 }
 
 
 /*********************************************
-      CREATE OUR DATABASE CLASS INSTANCE
-*********************************************/ 
-$cbsql = new Charbrowser_SQL($cb_host, $cb_user, $cb_pass, $cb_db);
+      CREATE OUR DATABASE CLASS INSTANCE(S)
+*********************************************/
+$cbsql = new Charbrowser_SQL($cb_host, $cb_user, $cb_pass, $cb_db, $cb_port);
 
 if ($cbsql->connect_error()) cb_message_die($language['MESSAGE_ERROR'], $language['MESSAGE_DB_NOCONNECT']);
+
+//do we have a seperate content DB?
+if ($cb_use_content_db)
+{
+   $cbsql_content = new Charbrowser_SQL($cb_content_host, $cb_content_user, $cb_content_pass, $cb_content_db, $cb_content_port);
+   if ($cbsql_content->connect_error()) cb_message_die($language['MESSAGE_ERROR'], $language['MESSAGE_DB_NOCONNECT']);
+}
+else
+{
+   $cbsql_content = $cbsql;
+}
 ?>
